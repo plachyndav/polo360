5d7b5bc000079fa92c34e209168c6988a0241975
Vitaliy <plachyndav@gmail.com>
2019-01-20 13:05:43 +0200

    linked pictures

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ button1.addEventListener("click", function(){
     button1.style.background = "linear-gradient(to top, #02b0e6 0%, #99dff5 100%)";
     button2.style.background = "linear-gradient(to top, #6d6c6c 0%, #ffffff 80%, #c9c9c9 100%)";
     
-    slider.style.background = 'no-repeat url("./img/slider1.jpg")';
+    slider.style.background = 'no-repeat url("https://www.dropbox.com/s/q2m8e22wpod0r5l/slider1.jpg?dl=1")';
 }, false)
 
 button2.addEventListener("click", function(){
     button2.style.background = "linear-gradient(to top, #02b0e6 0%, #99dff5 100%)";
     button1.style.background = "linear-gradient(to top, #6d6c6c 0%, #ffffff 80%, #c9c9c9 100%)";
     
-    slider.style.background = 'no-repeat url("./img/slider2.jpg")';
+    slider.style.background = 'no-repeat url("https://www.dropbox.com/s/zfikgili6bvc5mv/slider2.jpg?dl=1")';
 }, false)
